fbb393275f8da56cbe0e6bcce6ef5dfd5abcc4ee
Razvan Rotundu <raz.rotundu@gmail.com>
2026-01-21 12:49:39 -0500

    Create UserService Jpa implementation

--- src/main/java/com/example/authentication/service/UserServiceImpl.java
+++ src/main/java/com/example/authentication/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.authentication.service;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.authentication.dto.UserDto;
+import com.example.authentication.dto.utils.UserConverter;
 import com.example.authentication.entity.UserEntity;
 import com.example.authentication.repository.UserRepository;
 
@@ -16,6 +18,7 @@ import com.example.authentication.repository.UserRepository;
  */
 
 @Service
+@Qualifier("jpaImpl")
 public class UserServiceImpl implements UserService {
 	
 	private final UserRepository userRepository;
@@ -30,25 +33,36 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Optional<UserDto> createUser(String email, String password, String userType) {
-		// TODO Auto-generated method stub
+
+		// If user with email already exists, return empty optional
 		if(!userRepository.findByEmail(email).isEmpty()) {
 			return Optional.empty();
 		}
 		
-		UserEntity newUser = new UserEntity(UUID.randomUUID(), email, password, userType);
-		return Optional.of(userRepository.save(newUser));
+		UserEntity newUser = new UserEntity(UUID.randomUUID(),
+				email, 
+				passwordEncoder.encode(password), 
+				userType);
+		
+		return Optional.of(userRepository.save(newUser))
+				.map(UserConverter::toUserDto);
 	}
 
 	@Override
 	public Optional<UserDto> findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		
+		return userRepository.findByEmail(email)
+				.map(UserConverter::toUserDto);
 	}
 
 	@Override
 	public boolean validateUser(String email, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return userRepository.findByEmail(email)
+				.map(user ->
+						passwordEncoder.matches(password, user.getPassword()))
+				.orElse(false);
+
 	}
 
 }
